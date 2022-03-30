d77d6df67a50aadda97817e073aef6cf6c06b9de
David <1810493+djh82@users.noreply.github.com>
2022-03-30 22:21:43 +0100

    [ci skip]

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,10 @@
 module.exports = {
     plugins: {
-        '@fullhuman/postcss-purgecss': {
-            content: [
-                './layouts/**/*.html',
-                './static/js/*.js'
-              ],
-            whitelist: [
-            ]
+      '@fullhuman/postcss-purgecss': {
+        content: ['./hugo_stats.json'],
+        defaultExtractor: (content) => {
+            let els = JSON.parse(content).htmlElements;
+            return els.tags.concat(els.classes, els.ids);
         },
         autoprefixer: {},
         cssnano: {preset: 'default'}
