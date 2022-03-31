dd586b6e36d0a54e6b67ecdbbfcc72bac66f4f70
David <1810493+djh82@users.noreply.github.com>
2022-03-31 14:58:53 +0100

    fix css

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,7 @@ module.exports = {
     map: false,
     plugins: {
         '@fullhuman/postcss-purgecss': {
-            content: ['./hugo_stats.json'],
-            defaultExtractor: (content) => {
-                let els = JSON.parse(content).htmlElements;
-                return els.tags.concat(els.classes, els.ids);
-            }
+            content: ['./layouts/**/*.html'],
         },
         autoprefixer: {},
         cssnano: {preset: 'default'}
