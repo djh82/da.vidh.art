2c9bda0a5e0263639964f717f466162e96cb863f
David <1810493+djh82@users.noreply.github.com>
2022-03-31 12:47:37 +0100

    move favicons

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,14 @@
 module.exports = {
+    map: false,
     plugins: {
         '@fullhuman/postcss-purgecss': {
-            content: [
-                './layouts/**/*.html',
-                './static/js/*.js'
-              ],
-            whitelist: [
-            ]
+            content: ['./hugo_stats.json'],
+            defaultExtractor: (content) => {
+                let els = JSON.parse(content).htmlElements;
+                return els.tags.concat(els.classes, els.ids);
+            }
         },
         autoprefixer: {},
         cssnano: {preset: 'default'}
     }
-  };
+};
